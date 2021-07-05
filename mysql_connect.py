957146e3ca344f3e5e5eb7d094515ed49dbab893
Alexandre Pimentel <58637629+profxx@users.noreply.github.com>
2021-07-05 09:13:24 -0300

    Add files via upload

--- mysql_connect.py
+++ mysql_connect.py
@@ -2,28 +2,29 @@
 MySQL Connect class
 
 '''
-import mysql.connector as mysql
+import mysql.connector as mysql		# pip install mysql-connector-python
 
 class Funcs:
-	def conecta_bd(self):
-		try:
-			self.con = mysql.connect(
-				host = 'localhost',
-				database = 'my_books',
-				user = 'root',
-				passwd = '')
 
-			self.cursor = con.cursor()
+	def conecta_bd(self):					# CONEXÃO
+		try:
+			self.con = mysql.connect(		# Conectar
+				host = '127.0.0.1',			# -
+				database = 'my_books',		# -
+				user = 'root',				# -- Parâmetros necessários
+				passwd = '')				# - 
+
+			self.cursor = con.cursor()		# Criar cursor
 			print('Conectado ao banco de dados.')
 
 		except:
 			print('Falha ao conectar ao banco de dados.')
 
 	def desconecta_bd(self):
-		self.con.close()
+		self.con.close()					# Desconecta
 		print('Desconectou com sucesso.')
 
-	def cria_tabela(self, tabela):
+	def cria_tabela(self):					# Cria tabelas
 		self.conecta_bd()
 		self.cursor.execute('''
 				CREATE TABLE IF NOT EXISTS books(
@@ -33,7 +34,7 @@ class Funcs:
 					paginas INT
 				);
 			''')
-		self.con.commit()
+		self.con.commit()					# Salva a alteração
 		print('Banco de Dados criado com sucesso.')
 		self.desconecta_bd()
 
@@ -48,5 +49,10 @@ class Funcs:
 		self.cursor.execute("""
             INSERT INTO books(nome_livro, autor, paginas)
             VALUES (?, ?, ?);""", (self.nome_livro, self.autor, self.paginas))
-        self.con.commit()
-        self.desconecta_bd()
+		self.con.commit()
+		self.desconecta_bd()
+
+#Programa Principal
+a = Funcs()
+a.cria_tabela()
+a.insere_livro()
